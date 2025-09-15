f80839b9d1cab22ebf5fefdae6e6b584c4390f0f
Myxa <animekilla@proton.me>
2025-09-15 13:34:48 +0300

    move JEI mixin to a separate file

--- src/main/java/ua/myxazaur/lemonskin/LemonSkinLateLoader.java
+++ src/main/java/ua/myxazaur/lemonskin/LemonSkinLateLoader.java
@@ -1,5 +1,6 @@
 package ua.myxazaur.lemonskin;
 
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
 import zone.rong.mixinbooter.ILateMixinLoader;
 
@@ -7,8 +8,22 @@ import java.util.Collections;
 import java.util.List;
 
 @Optional.Interface(modid="mixinbooter", iface="zone.rong.mixinbooter.ILateMixinLoader")
-public class LemonSkinLateLoader implements ILateMixinLoader {
-    public List<String> getMixinConfigs() {
-        return Collections.singletonList("mixins.lemonskin.json");
+public class LemonSkinLateLoader implements ILateMixinLoader
+{
+    public List<String> getMixinConfigs()
+    {
+        return Collections.singletonList("mixins.jei_compat.json");
+    }
+
+    @Override
+    public boolean shouldMixinConfigQueue(String mixinConfig)
+    {
+        switch (mixinConfig)
+        {
+            case "mixins.jei_compat.json":
+                Loader.isModLoaded("jei");
+            default:
+                return true;
+        }
     }
 }
