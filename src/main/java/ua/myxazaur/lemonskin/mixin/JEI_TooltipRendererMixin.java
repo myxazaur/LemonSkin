08d5bbfbd5906014409db5bb976141cbc52eaab9
Myxa <animekilla@proton.me>
2025-09-16 17:43:01 +0300

    jei tooltip mixin hotfix

--- src/main/java/ua/myxazaur/lemonskin/mixin/JEI_TooltipRendererMixin.java
+++ src/main/java/ua/myxazaur/lemonskin/mixin/JEI_TooltipRendererMixin.java
@@ -8,9 +8,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ua.myxazaur.lemonskin.LemonSkin;
 import ua.myxazaur.lemonskin.ModConfig;
 import ua.myxazaur.lemonskin.helpers.TooltipHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(value = TooltipRenderer.class, remap = false)
@@ -20,8 +22,22 @@ public abstract class JEI_TooltipRendererMixin
     at = @At("HEAD"), remap = false)
     private static void drawHoveringText(ItemStack itemStack, Minecraft minecraft, List<String> textLines, int x, int y, int maxWidth, FontRenderer font, CallbackInfo ci)
     {
-        if (!ModConfig.CLIENT.USE_MODERN_TOOLTIP) return;
-        textLines.removeIf(line -> line.contains("\u00A0"));
-        TooltipHelper.reserveFoodTooltipSpace(textLines, itemStack);
+        try {
+            if (!ModConfig.CLIENT.USE_MODERN_TOOLTIP) return;
+            if (textLines != null && !textLines.isEmpty()) {
+                List<String> mutable = new ArrayList<>(textLines);
+                mutable.removeIf(line -> line != null && line.contains("\u00A0"));
+
+                if (textLines.getClass().getName().contains("java.util.Collections")) textLines = mutable;
+                else {
+                    textLines.clear();
+                    textLines.addAll(mutable);
+                }
+
+                TooltipHelper.reserveFoodTooltipSpace(textLines, itemStack);
+            }
+        } catch (Exception e) {
+            LemonSkin.Log.error("Failed to process JEI tooltip", e);
+        }
     }
 }
