a4f5ca745ad28efcd72b5586cdcff5834d26613c
Myxa <animekilla@proton.me>
2025-09-19 11:41:40 +0300

    use reload listener for gray icons

--- src/main/java/ua/myxazaur/lemonskin/LemonSkin.java
+++ src/main/java/ua/myxazaur/lemonskin/LemonSkin.java
@@ -1,26 +1,25 @@
 package ua.myxazaur.lemonskin;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.resources.IReloadableResourceManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ua.myxazaur.lemonskin.client.DebugInfoHandler;
+import ua.myxazaur.lemonskin.client.GrayIconsReloader;
 import ua.myxazaur.lemonskin.client.HUDOverlayHandler;
 import ua.myxazaur.lemonskin.client.TooltipOverlayHandler;
 import ua.myxazaur.lemonskin.helpers.BetterWithModsHelper;
 import ua.myxazaur.lemonskin.network.SyncHandler;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class LemonSkin {
@@ -49,47 +48,11 @@ public class LemonSkin {
 
         if (event.getSide() == Side.CLIENT)
         {
-            generateGrayIcons();
             DebugInfoHandler.init();
             HUDOverlayHandler.init();
             TooltipOverlayHandler.init();
+
+            ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener(new GrayIconsReloader());
         }
-    }
-
-    private static void generateGrayIcons()
-    {
-        Minecraft mc = Minecraft.getMinecraft();
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(mc.getResourceManager().getResource(new ResourceLocation(Tags.MOD_ID, "textures/icons.png")).getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        DynamicTexture grayTex = new DynamicTexture(toGrayscale(image));
-        ResourceLocation rl = new ResourceLocation(Tags.MOD_ID, "icons_gray");
-        mc.getTextureManager().loadTexture(rl, grayTex);
-        grayIcons = rl;
-    }
-
-    private static BufferedImage toGrayscale(BufferedImage img) {
-        int width = img.getWidth();
-        int height = img.getHeight();
-
-        BufferedImage grayImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int rgb = img.getRGB(x, y);
-
-                Color c = new Color(rgb, true);
-                int gray = (int)(0.299 * c.getRed() + 0.587 * c.getGreen() + 0.114 * c.getBlue());
-
-                Color newColor = new Color(gray, gray, gray, c.getAlpha());
-                grayImg.setRGB(x, y, newColor.getRGB());
-            }
-        }
-
-        return grayImg;
     }
 }
---
+++ src/main/java/ua/myxazaur/lemonskin/client/GrayIconsReloader.java
@@ -0,0 +1,69 @@
+package ua.myxazaur.lemonskin.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.IResourceManagerReloadListener;
+import net.minecraft.util.ResourceLocation;
+import ua.myxazaur.lemonskin.LemonSkin;
+import ua.myxazaur.lemonskin.Tags;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Gray icons are used for customizable saturation color
+ * @see ua.myxazaur.lemonskin.ModConfig
+ */
+public class GrayIconsReloader implements IResourceManagerReloadListener
+{
+    @Override
+    public void onResourceManagerReload(IResourceManager resourceManager) {
+        generateGrayIcons(resourceManager);
+    }
+
+    private void generateGrayIcons(IResourceManager resourceManager)
+    {
+        try {
+            BufferedImage image = ImageIO.read(
+                    resourceManager.getResource(
+                            new ResourceLocation(Tags.MOD_ID, "textures/icons.png")
+                    ).getInputStream()
+            );
+
+            DynamicTexture grayTex = new DynamicTexture(toGrayscale(image));
+            ResourceLocation rl = new ResourceLocation(Tags.MOD_ID, "icons_gray");
+
+            Minecraft.getMinecraft().getTextureManager().loadTexture(rl, grayTex);
+            LemonSkin.grayIcons = rl;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to reload gray icons", e);
+        }
+    }
+
+    private static BufferedImage toGrayscale(BufferedImage img)
+    {
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        BufferedImage grayImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = img.getRGB(x, y);
+
+                Color c = new Color(rgb, true);
+                int gray = (int)(0.299 * c.getRed() + 0.587 * c.getGreen() + 0.114 * c.getBlue());
+
+                Color newColor = new Color(gray, gray, gray, c.getAlpha());
+                grayImg.setRGB(x, y, newColor.getRGB());
+            }
+        }
+
+        return grayImg;
+    }
+}
+
