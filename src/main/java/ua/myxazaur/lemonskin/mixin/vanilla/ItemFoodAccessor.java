47a72edd1cb13030ba4781ceb23e59826fb761b9
Myxa <animekilla@proton.me>
2025-09-23 13:15:22 +0300

    use accessor instead of reflection

--- src/main/java/ua/myxazaur/lemonskin/mixin/vanilla/ItemFoodAccessor.java
+++ src/main/java/ua/myxazaur/lemonskin/mixin/vanilla/ItemFoodAccessor.java
@@ -9,5 +9,5 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 public interface ItemFoodAccessor
 {
     @Accessor("potionId")
-    public PotionEffect getPotionId();
+    PotionEffect getPotionId();
 }
